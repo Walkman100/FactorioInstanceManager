583d4e08e472879468acd53083db4d257b68a714
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2021-03-16 15:40:49 +0200

    renamed LICENSE to LICENSE.md, added project files

---
+++ FactorioInstanceManager.vb
@@ -0,0 +1,10 @@
+Public Partial Class FactorioInstanceManager
+    Public Sub New()
+        ' The Me.InitializeComponent call is required for Windows Forms designer support.
+        Me.InitializeComponent()
+        
+        '
+        ' TODO : Add constructor code after InitializeComponents
+        '
+    End Sub
+End Class
