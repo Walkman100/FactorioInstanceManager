7381a0a254b6355f7127f4f124b65e256dac9e75
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2021-03-17 14:44:46 +0200

    added Ookii InputBox and FolderBrowserDialog functions to Helpers

--- Helpers.vb
+++ Helpers.vb
@@ -23,6 +23,86 @@ Public Class Helpers
     Friend Shared Function GetOS() As OS
         Return If(Environment.GetEnvironmentVariable("OS") = "Windows_NT", OS.Windows, OS.Other)
     End Function
+
+    Public Shared Function GetInput(ByRef input As String, Optional windowTitle As String = "",
+                                    Optional header As String = "", Optional content As String = Nothing) As DialogResult
+        If OokiiDialogsLoaded() Then
+            Return OokiiInputBox(input, windowTitle, header, content)
+        Else
+            Dim inputBoxPrompt As String = header
+            If content IsNot Nothing Then
+                inputBoxPrompt &= Environment.NewLine & content
+            End If
+
+            input = Microsoft.VisualBasic.InputBox(inputBoxPrompt, windowTitle, input)
+            If String.IsNullOrEmpty(input) Then
+                Return DialogResult.Cancel
+            Else
+                Return DialogResult.OK
+            End If
+        End If
+    End Function
+    Private Shared Function OokiiInputBox(ByRef input As String, Optional windowTitle As String = "",
+                                          Optional header As String = "", Optional content As String = "") As DialogResult
+        Dim ooInput As New Ookii.Dialogs.InputDialog With {
+            .Input = input,
+            .WindowTitle = windowTitle,
+            .MainInstruction = header,
+            .Content = content
+        }
+
+        Dim returnResult = ooInput.ShowDialog(FactorioInstanceManager)
+        input = ooInput.Input
+        Return returnResult
+    End Function
+
+    Public Shared Function SelectFolderDialog(ByRef selectedPath As String, Optional description As String = "",
+                                              Optional showNewFolderButton As Boolean = True) As DialogResult
+        If OokiiDialogsLoaded() Then
+            Return OokiiFolderBrowserDialog(selectedPath, description, showNewFolderButton)
+        Else
+            Dim fbd As New FolderBrowserDialog() With {
+                .SelectedPath = selectedPath,
+                .Description = description,
+                .ShowNewFolderButton = showNewFolderButton
+            }
+            Try
+                Return fbd.ShowDialog()
+            Finally
+                selectedPath = fbd.SelectedPath
+            End Try
+        End If
+    End Function
+    Private Shared Function OokiiFolderBrowserDialog(ByRef selectedPath As String, Optional description As String = "",
+                                                     Optional showNewFolderButton As Boolean = True) As DialogResult
+        Dim fbd As New Ookii.Dialogs.VistaFolderBrowserDialog() With {
+            .SelectedPath = selectedPath,
+            .Description = description,
+            .ShowNewFolderButton = showNewFolderButton,
+            .UseDescriptionForTitle = True
+        }
+        Try
+            Return fbd.ShowDialog()
+        Finally
+            selectedPath = fbd.SelectedPath
+        End Try
+    End Function
+
+    Private Shared Function OokiiDialogsLoaded() As Boolean
+        Try
+            OokiiDialogsLoadedDelegate()
+            Return True
+        Catch ex As FileNotFoundException When ex.FileName.StartsWith("Ookii.Dialogs")
+            Return False
+        Catch ex As Exception
+            MessageBox.Show("Unexpected error loading Ookii.Dialogs.dll!" & Environment.NewLine & Environment.NewLine & ex.Message,
+                            "Unexpected Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Return False
+        End Try
+    End Function
+    Private Shared Sub OokiiDialogsLoadedDelegate() ' because calling a not found class will fail the caller of the method not directly in the method
+        Dim test = Ookii.Dialogs.TaskDialogIcon.Information
+    End Sub
 End Class
 
 Module Extensions
