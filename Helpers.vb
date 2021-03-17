543fa14a606cf1fad9e6b9d4d7d47dedda5e1b5e
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2021-03-17 14:10:10 +0200

    added Helpers.GetOS and DoubleBuffered extension

--- Helpers.vb
+++ Helpers.vb
@@ -1,5 +1,14 @@
+Imports System
 Imports System.Collections.Generic
 Imports System.IO
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Windows.Forms
+
+Enum OS
+    Windows
+    Other
+End Enum
 
 Public Class Helpers
     Public Shared Sub WriteAllLines(path As String, lines As IEnumerable(Of String), separator As String)
@@ -10,4 +19,16 @@ Public Class Helpers
             Next
         End Using
     End Sub
+
+    Friend Shared Function GetOS() As OS
+        Return If(Environment.GetEnvironmentVariable("OS") = "Windows_NT", OS.Windows, OS.Other)
+    End Function
 End Class
+
+Module Extensions
+    <Extension()>
+    Public Sub DoubleBuffered(control As Control, enable As Boolean) ' thanks to https://stackoverflow.com/a/15268338/2999220
+        Dim doubleBufferPropertyInfo = control.[GetType]().GetProperty("DoubleBuffered", BindingFlags.Instance Or BindingFlags.NonPublic)
+        doubleBufferPropertyInfo.SetValue(control, enable, Nothing)
+    End Sub
+End Module
