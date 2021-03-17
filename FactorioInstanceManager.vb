ab129aa11476149c3ec97d7219444c0e1d38784e
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2021-03-17 17:46:45 +0200

    added install/instance helpers, added loading settings into form

--- FactorioInstanceManager.vb
+++ FactorioInstanceManager.vb
@@ -7,6 +7,19 @@ Public Class FactorioInstanceManager
         lstInstances.DoubleBuffered(True)
 
         Settings.Init()
+
+        Me.Width = Settings.WindowWidth
+        Me.Height = Settings.WindowHeight
+        Me.CenterToParent()
+
+        If Settings.WindowMaximised Then Me.WindowState = FormWindowState.Maximized
+
+        For Each install As Settings.Install In Settings.Installs
+            lstInstalls.Items.Add(CreateInstallItem(install))
+        Next
+        For Each instance As Settings.Instance In Settings.Instances
+            lstInstances.Items.Add(CreateInstanceItem(instance))
+        Next
     End Sub
 
     Private Sub FactorioInstanceManager_Resize() Handles MyBase.Resize
@@ -15,29 +28,39 @@ Public Class FactorioInstanceManager
 
 #Region "Install Helpers"
     Public Shared Function GetInstall(item As ListViewItem) As Settings.Install
-
+        Return DirectCast(item.Tag, Settings.Install)
     End Function
 
     Public Shared Function CreateInstallItem(install As Settings.Install) As ListViewItem
-
+        Return UpdateInstallItem(New ListViewItem({"", "", ""}), install)
     End Function
 
     Public Shared Function UpdateInstallItem(item As ListViewItem, install As Settings.Install) As ListViewItem
-
+        item.SubItems(0).Text = install.Path
+        item.SubItems(1).Text = install.Version?.ToString()
+        Try : item.SubItems(2).Text = General.GetInstallCurrentInstance(install.Path)
+        Catch ex As Exception : item.SubItems(2).Text = "Error: " & ex.Message
+        End Try
+        item.Tag = install
+        Return item
     End Function
 #End Region
 
 #Region "Instance Helpers"
     Public Shared Function GetInstance(item As ListViewItem) As Settings.Instance
-
+        Return DirectCast(item.Tag, Settings.Instance)
     End Function
 
     Public Shared Function CreateInstanceItem(instance As Settings.Instance) As ListViewItem
-
+        Return UpdateInstanceItem(New ListViewItem({"", "", ""}), instance)
     End Function
 
     Public Shared Function UpdateInstanceItem(item As ListViewItem, instance As Settings.Instance) As ListViewItem
-
+        item.SubItems(0).Text = instance.Path
+        item.SubItems(1).Text = instance.Version?.ToString()
+        item.SubItems(2).Text = instance.IconPath
+        item.Tag = instance
+        Return item
     End Function
 #End Region
 
